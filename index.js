0b316e7822cb278b8f0c9eeb6e766ba542e804c4
momo <callmedevmomo@gmail.com>
2020-03-02 15:02:16 +0900

    optional parameter

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("hey");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "momo", age = 27, gender = "male";
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+sayHi(name, age);
+//# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("hey")
+const name = "momo",
+  age = 27,
+  gender = "male";
+
+const sayHi = (name, age, gender?) => {
+  //gender ==> optional
+  console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+
+sayHi(name, age);
+
+export {}; //bug fix

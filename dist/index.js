9ad0a1b18f5c4244323d3d89900dbf4832799a0b
momo <callmedevmomo@gmail.com>
2020-03-02 17:17:56 +0900

    Creating a Block

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const CryptoJS = require("crypto-js");
 class Block {
     constructor(index, hash, previousHash, data, timestamp) {
         this.index = index;
@@ -9,7 +10,10 @@ class Block {
         this.timestamp = timestamp;
     }
 }
+Block.cacluateBlockHash = (index, previousHash, timestamp, data) => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
 const genesisBlock = new Block(0, "1241240adfasdf", "", "HELLO", 123456);
 let blockChain = [genesisBlock];
-console.log(blockChain);
+const getBlockchain = () => blockChain;
+const getLatestBlock = () => blockChain[blockChain.length - 1];
+const getNewTimeStamp = () => Math.round(new Date().getTime() / 1000);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
+import * as CryptoJS from "crypto-js";
+
 class Block {
   public index: number;
   public hash: string;
   public previousHash: string;
   public data: string;
   public timestamp: number;
+  static cacluateBlockHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string
+  ): string =>
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
   constructor(index, hash, previousHash, data, timestamp) {
     this.index = index;
     this.hash = hash;
@@ -12,10 +21,11 @@ class Block {
     this.timestamp = timestamp;
   }
 }
-
 const genesisBlock: Block = new Block(0, "1241240adfasdf", "", "HELLO", 123456);
 
-let blockChain: [Block] = [genesisBlock];
+let blockChain: Block[] = [genesisBlock];
 
-console.log(blockChain);
+const getBlockchain = (): Block[] => blockChain;
+const getLatestBlock = (): Block => blockChain[blockChain.length - 1];
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
 export {}; //bug fix
